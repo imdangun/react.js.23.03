5156ccf660a3aed910a6bcb0dbc3ce8b3035efbc
a <a@a.com>
2023-03-17 18:28:47 +0900

    a

---
+++ src/4.hook/App.js
@@ -0,0 +1,7 @@
+import MyUseCallback from './1.useCallback'
+
+export default function App() {
+    return (
+        <MyUseCallback/>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import ReactDOM from 'react-dom/client'
 //1. import App from './1.basic/App'
 //2. import App from './2.tictactoe/App'
-import App from './3.thinking/App'
+//3. import App from './3.thinking/App'
+import App from './4.hook/App'
 
 ReactDOM.createRoot(
     document.getElementById('root')
