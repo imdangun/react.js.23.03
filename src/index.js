d824619538bd4f5ce10f4c4a79338c3a02e53367
a <a@a.com>
2023-03-17 17:18:14 +0900

    a

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import ReactDOM from 'react-dom/client'
 //1. import App from './1.basic/App'
-import App from './2.tictactoe/App'
+//2. import App from './2.tictactoe/App'
+import App from './3.thinking/App'
 
 ReactDOM.createRoot(
     document.getElementById('root')
